10d41b67bc9e049884f5dddfedde26cb330d628e
Rueded <rensyan95@gmail.com>
2026-05-11 18:38:38 +0800

    improved ui.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,15 +7,14 @@ plugins {
 
 android {
     namespace = "com.example.mymusic"
-    // 👇 跟随最新时代，直接拉到 API 37
     compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.mymusic"
         minSdk = 26
-        targetSdk = 36 // 👇 目标也对齐 37
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 36
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
